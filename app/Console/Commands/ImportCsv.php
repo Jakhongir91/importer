61c970d87f5a57d56bbd803fe583929ac95a00a6
Jakhongir <jakhongir.nishanbaev@gmail.com>
2023-10-18 16:54:29 +0500

    feat: import large csv file

---
+++ app/Console/Commands/ImportCsv.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Domain\Import\Job\ImportCsv as ImportCsvJob;
+use Illuminate\Console\Command;
+
+class ImportCsv extends Command
+{
+    protected $signature = 'import:csv';
+    protected $description = 'Imports large CSV file.';
+
+    public function handle()
+    {
+        $filePath = __DIR__ . "/../../../dump/large-1mln-rows.csv";
+        ImportCsvJob::dispatch($filePath);
+    }
+}
---
+++ app/Domain/Import/Job/ImportCsv.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Domain\Import\Job;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class ImportCsv implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    private string $filePath;
+
+    public function __construct(string $filePath)
+    {
+        $this->filePath = $filePath;
+    }
+
+    public function handle()
+    {
+        $fileStream = fopen($this->filePath, "r");
+
+        $skipFirstRow = true;
+        while (($line = fgetcsv($fileStream)) !== false) {
+            if ($skipFirstRow) {
+                $skipFirstRow = false;
+                continue;
+            }
+
+            ImportCsvRow::dispatch($line);
+        }
+    }
+}
