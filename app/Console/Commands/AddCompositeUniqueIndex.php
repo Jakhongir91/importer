d3d369355bc56a2f0a23cea6b036fa195df8108d
jakhongir <jakhongir.nishanbaev@gmail.com>
2023-10-30 13:51:49 +0500

    fix: add year to composite unique index

--- app/Console/Commands/AddCompositeUniqueIndex.php
+++ app/Console/Commands/AddCompositeUniqueIndex.php
@@ -7,23 +7,23 @@ use Illuminate\Support\Facades\DB;
 
 class AddCompositeUniqueIndex extends Command
 {
-    protected $signature = "incomes:add-unique-index";
     protected $description = "Add unique composite index";
 
     public function handle()
     {
-        $result = DB::select("select inps, income, count(*)
+        $result = DB::select("select inps, income, extract(year from date), extract(month from date), count(*)
                 from incomes
-                group by inps, income
+                group by inps, income, extract(year from date), extract(month from date)
                 HAVING count(*) > 1;");
 
         if (count($result) > 0) {
+            $this->info(print_r($result, true));
             $this->info("Duplicates in incomes table should be deleted first.");
             return;
         }
 
         DB::statement("create unique index date_inps_income_unique
-                on incomes (extract(month from date), inps, income);");
+                on incomes (extract(year from date), extract(month from date), inps, income);");
 
         $this->info("Successfully added composite unique index.");
     }
